fefde1c207406081301ccd39c8488f8599b439d3
Noah Kreiger <noahkreiger@Noahs-MacBook-Pro.local>
2020-01-30 21:42:58 -0500

    initial commit

---
+++ pages/customLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-row column justify-center align-center>
+    <v-col>
+      <h1>Inside my custom view!</h1>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'CustomLayout',
+  layout: 'custom'
+}
+</script>
